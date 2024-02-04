4c6eeb17e9f06ad265d60f852237d60133dbcab8
mavart2 <mavart08@gmail.com>
2024-02-04 17:49:21 -0300

    Primer commit en la rama main

---
+++ js/app.js
@@ -0,0 +1,140 @@
+
+// console.log(document.title);
+// document.title='INSISTO EN EL COLOR';
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.all[11]);
+// console.log(document.forms[0]);
+
+const btn =  document.getElementById("agregar")
+  const container = document.getElementById("container")
+  
+  btn.addEventListener("click", ()=>{
+  
+      let nombre = prompt("Escribe tu nombre")
+      let tarea = prompt("Tu actividad")
+  
+  
+  const newCard =  document.createElement("div")
+  newCard.innerHTML= `
+  <h2>Hola ${nombre}</h2>
+  <p>${tarea}</p>
+  `
+  container.appendChild(newCard)
+  })
+
+//getElementById()
+console.log(document.getElementById('header-title'))
+let headerTitle = document.getElementById('header-title')
+
+headerTitle.textContent='Insistir en el color';
+console.log(headerTitle)
+//let items = document.getElementsByClassName('list-group-item')
+//mainTitle = 'Agregar color';
+let items = document.getElementsByTagName('li')
+console.log(items[0]);
+// items[0].textContent='Amarillo';
+// items[1].textContent='rojo';
+
+//querySelector
+let bajo = document.querySelector('#main-header')
+bajo.style.borderBottom= 'solid 4px #ccc'
+
+// let input = document.querySelector('input')
+// input.value = 'Agrega Colores o lo que quieras...'
+
+let submit = document.querySelector('input[type ="submit"]')
+submit.value='Subir'
+
+// let item = document.querySelectorAll('.list-group-item')
+// item[1].style.color='red'
+// console.log(items[3])
+
+ var itemList = document.querySelector('#items')
+ var main = itemList.parentNode;
+main.style.backgroundColor = 'white'
+
+let titulos = document.querySelectorAll('.title')
+titulos[0].textContent='Agrega Colores o lo que quieras...'
+
+let boton = document.getElementById("agregar");   //crear un boton en html
+boton.addEventListener("click", function() {
+    console.log("¡Hiciste clic en el botón!");
+    alert("evento detectado, mira la consola")
+  });
+
+//agregar colores o elementos a la lista
+
+    let form = document.getElementById('formAgregar')
+    let lista = document.getElementById('items')
+  
+    main.addEventListener('submit', agregarItem);
+  //evento click de la lista
+  lista.addEventListener('click', eliminarItem)
+
+    function agregarItem(e) {
+      e.preventDefault();
+      let newItem = document.getElementById('item').value;
+      
+      if (newItem.trim()!=''){
+      let li = document.createElement('li');
+      li.className = 'list-group-item';
+      li.appendChild(document.createTextNode(newItem)); 
+      lista.appendChild(li);
+
+      // Verifica si el valor es "azul" y agrega el complementario
+    if (newItem.toLowerCase() === 'azul') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es anaranjado'));
+    }else if (newItem.toLowerCase() === 'rojo') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es verde'));
+    } else if (newItem.toLowerCase() === 'amarillo') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es violeta'));
+    }else if (newItem.toLowerCase() === 'anaranjado') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es azul'));
+    } else if (newItem.toLowerCase() === 'verde') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es rojo'));
+    }else if (newItem.toLowerCase() === 'violeta') {
+      li.appendChild(document.createElement('br')); // Agrega un salto de línea
+      li.appendChild(document.createTextNode('su complementario es amarillo'));
+    }   
+    else {
+      // Manejar el caso donde newItem está vacío
+      alert('Sugerencia: Ingresa un color primario o secundario.');
+    }
+
+      let boton = document.createElement('button')
+      boton.className= 'btn btn-danger btn-sm float-right eliminar'
+      boton.appendChild(document.createTextNode('x'))
+      li.appendChild(boton)
+
+      
+
+       // Limpiar el campo de búsqueda después de agregar un elemento (puedes ajustar según tus necesidades)
+      document.getElementById('item').value = '';
+    }else {
+      alert('Ingresa un elemento válido.');
+    }
+ 
+    }
+
+    function eliminarItem(e){
+        
+        if(e.target.classList.contains('eliminar')){
+            let li = e.target.parentElement;
+            lista.removeChild(li);
+
+        }
+        
+
+    }
+    
+    
+
+
+
+
